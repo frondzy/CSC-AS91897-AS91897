8d4c9868002698539d9dddedf0165256f559bd93
Krishan Chand <chandk2358@gmail.com>
2022-06-24 19:12:09 +1200

    creating buttons

--- CSC - AS91897.py
+++ CSC - AS91897.py
@@ -16,3 +16,29 @@ root = Tk()
 root.title("Julie's Party Hire Item Tracker")
 root.geometry('1000x850')
 
+# using global variables
+global item_details, entry1, entry2, entry3, entry4, entry5, total_entries, row_count
+total_entries = 0
+item_details = []
+row_count = 0 
+
+# Creating button commands
+def print():
+    global item_details, entry1, entry2, entry3, entry4, entry5, total_entries, row_count
+    
+    while row_count < total_entries:
+        Label(root, text = row_count).grid(column=0,row=row_count+9)
+        Label(root, text="                                    "+(item_details[row_count][0])+"                                   ").grid(column=1,row=row_count+9)
+        Label(root, text="                                    "+(item_details[row_count][1])+"                                   ").grid(column=2,row=row_count+9)
+        Label(root, text="                                    "+(item_details[row_count][2])+"                                   ").grid(column=3,row=row_count+9)
+        Label(root, text="                                    "+(item_details[row_count][3])+"                                   ").grid(column=4,row=row_count+9)
+        row_count += 1 
+
+def quit():
+    root.destroy()
+
+def check(data, data_type):
+    try:
+        data_type(data)
+        return Type 
+        
