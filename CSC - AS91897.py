cde1415cd437ec692a954a72d579921a45eb62a4
Krishan Chand <chandk2358@gmail.com>
2022-06-24 15:58:20 +1200

    creating window

--- CSC - AS91897.py
+++ CSC - AS91897.py
@@ -1,1 +1,18 @@
-print('Hello world')
+# Created by: Krishan Chand
+
+#######################################################################
+#  This program is for Julie's Party Hire company to track her items  #
+#######################################################################
+
+
+# import tkinter 
+from tkinter import *
+from tkinter import ttk 
+from tkinter import messagebox
+import sqlite3
+
+# Creating the screen and titling 
+root = Tk()
+root.title("Julie's Party Hire Item Tracker")
+root.geometry('1000x850')
+
